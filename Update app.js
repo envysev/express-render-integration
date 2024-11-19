3fcccf43463520ff9b75a96e05cdb54407a1b168
envysev <alcedoluis4@gmail.com>
2024-11-19 12:02:44 +0800

    Add product-service

---
+++ Update app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const productService = require('./product-service');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/products', productService);
+
+app.listen(port, () => {
+    console.log(`Server running at http:/localhost:${port}`
+);
+});
